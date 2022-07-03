7f3f434beac59e9dc64dd61b98c03794a2aae94c
Gerrit Elenbaas <84500222+Gelenhaus@users.noreply.github.com>
2022-07-02 22:09:03 -0600

    making a carousel api

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@
 import './App.css';
 import occipital from '../src/assets/occipital/occipital.png';
 import frontal from '../src/assets/frontal/frontal.jpg'
+import temporal from '../src/assets/temporal/temporal.jpg'
 import EasyCarousal from 'carousal-hub';
+import cerebellum from '../src/assets/cerebellum/cerebellum.jpg';
+
 
 function App() {
   return (
@@ -21,15 +24,21 @@ function App() {
           </div>
 
           <div className="items full-width text-center">
-            <h1 className="product">3</h1>
+            <h1 className="product">
+              <img src={temporal}></img>
+            </h1>
           </div>
 
           <div className="items full-width text-center">
-            <h1 className="product">Temporal</h1>
+            <h1 className="product">
+              <img src={cerebellum}></img>
+            </h1>
           </div>
 
           <div className="items full-width text-center">
-            <h1 className="product">5</h1>
+            <h1 className="product">
+              <img src={frontal}>
+              </img></h1>
           </div>
         </EasyCarousal>
 
