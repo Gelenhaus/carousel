2263cd6a9c57b2a5e566d16829d91762967b2e8b
Gerrit Elenbaas <84500222+Gelenhaus@users.noreply.github.com>
2022-07-03 10:01:41 -0600

    trying to make the titles not be covered

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import frontal from '../src/assets/frontal/frontal.jpg'
 import temporal from '../src/assets/temporal/temporal.jpg'
 import EasyCarousal from 'carousal-hub';
 import cerebellum from '../src/assets/cerebellum/cerebellum.jpg';
-
+import spinalcord from '../src/assets/spinalcord/spinalcord.jpg';
 
 function App() {
   return (
@@ -16,29 +16,34 @@ function App() {
             <h1 className="product">
               <img src={occipital}></img>
             </h1>
+            <p> This is the occipital lobe</p>
           </div>
 
           <div className="items full-width text-center">
             <h1 className="product">
               <img src={frontal}></img> 2</h1>
+            <p>This is the frontal lobe.</p>
           </div>
 
           <div className="items full-width text-center">
             <h1 className="product">
               <img src={temporal}></img>
             </h1>
+            <p>Hello this is the temporal lobe</p>
           </div>
 
           <div className="items full-width text-center">
             <h1 className="product">
               <img src={cerebellum}></img>
+              <p>hello this is the cerebellum</p>
             </h1>
           </div>
 
           <div className="items full-width text-center">
             <h1 className="product">
-              <img src={frontal}>
-              </img></h1>
+              <img src={spinalcord}>
+              </img>
+              <p>hello this is the spinal cord</p></h1>
           </div>
         </EasyCarousal>
 
